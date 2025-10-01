e5b192b8af0d2dbc112bb84297e8680c88640f33
shreyajampana <156953760+shreyajampana@users.noreply.github.com>
2025-09-30 18:19:09 -0700

    Update STM32L432KC_TIM.c

--- mcu/lib/STM32L432KC_TIM.c
+++ mcu/lib/STM32L432KC_TIM.c
@@ -1,95 +1,96 @@
 // STM32L432KC_TIM.c
 // Source code for TIM functions
 
-#include STM32L432KC_TIM.h
+#include "STM32L432KC_TIM.h"
 
 
-void initTIM(TIM_TypeDef * TIM15) {
+void initTIM(TIM_TypeDef * TIMx) {
     //Set the prescaler
-    TIM15->PSC = 800;
+    TIMx->PSC = 7999;
 
     //Setting the auto-reload register to buffer
-    TIM15->CR1 |= (1 << 7);
+    TIMx->CR1 |= (1 << 7);
 
     //Set the UG
-    TIM15->EGR |= (1 << 0);
+    TIMx->EGR |= (1 << 0);
 
     //Set the CEN in CR1 - turn on the timer at the end
-    TIM15->CR1 |= (1 << 0);
+    TIMx->CR1 |= (1 << 0);
 }
 
 
-void delay_millis(TIM_TypeDef * TIM15, uint32_2 ms) {
+void delay_millis(TIM_TypeDef * TIMx, uint32_t ms) {
     //Set the ARR
-    TIM15->ARR = (TICKS_PER_MS * ms);
-
-    //Setting the counter to 0 to begin with
-    TIM15->CNT = 0;
-    
-    //Clear the update interrupt flag once the counter is reset
-    TIM15->SR &= ~(1 << 0);
+    TIMx->ARR = (TICKS_PER_MS_DELAY * ms);
 
     //Setting the UG to start the counter and upload the ARR value
-    TIM15->EGR |= (1 << 0); 
+    TIMx->EGR |= (1 << 0); 
+
+    //Setting the counter to 0 to begin with
+    TIMx->CNT = 0;
+    
+    //Clear the update interrupt flag once the counter is reset
+    TIMx->SR &= ~(1 << 0);
 
     //Wait until the interrupt flag goes high again
-    while (!(TIM15->SR & (1<<0)));
+    while (!(TIMx->SR & (1<<0)));
 
     //Clear the update interrupt flag
-    TIM15->SR &= ~(1 << 0);
+    TIMx->SR &= ~(1 << 0);
 }
 
 
-void initPWM(TIM_TypeDef * TIM16) {
+void initPWM(TIM_TypeDef * TIMx) {
     //Set the prescaler
-    TIM16->PSC = 7;
+    TIMx->PSC = 7;
     
     //Selecting the PWM mode 1
-    TIM16->CCMR1 |= (0b110 << 4);
+    TIMx->CCMR1 |= (0b110 << 4);
     
     //Enabling the preload register
-    TIM16->CCMR1 |= (1 << 3);
-
-    //Set the auto reload preload register
-    TIM16->CR1 |= (1 << 7);
+    TIMx->CCMR1 |= (1 << 3);
 
     //Setting OC1 polarity
-    TIM16->CCER &= ~(1 << 1);
+    TIMx->CCER &= ~(1 << 1);
+
+    //Set the auto reload preload register
+    TIMx->CR1 |= (1 << 7);
 
     //Enabling OCx output with a combination of bits found in two different registers
-    TIM16->CCER |= (1 << 0); //CC1E set to 1
+    TIMx->CCER |= (1 << 0); //CC1E set to 1
     //TIM16->CCER |= (1 << 2); //CC1NE set to 1
     //TIM16->BDTR |= (1 << 11); //OSSR set to 1
-    TIM16->BDTR |= (1 << 15); //MOE set to 1
+    TIMx->BDTR |= (1 << 15); //MOE set to 1
 
     //Setting the UG but to upload shadow registers
-    TIM16->EGR |= (1 << 0);
+    TIMx->EGR |= (1 << 0);
 
     //Set the CEN in CR1 - turn on the timer at the end
-    TIM16->CR1 |= (1 << 0);
+    TIMx->CR1 |= (1 << 0);
 }
 
 
 
-void setFreq(TIM_TypeDef * TIM16, uint32_2 freq){
+void setFreq(TIM_TypeDef * TIMx, uint32_t freq){
     //Calculating the arr and ccr values
-    uint32_t arr = (1/freq)*(1000)*TICKS_PER_MS;
+    uint32_t arr = (1000)*TICKS_PER_MS_FREQ/freq;
     uint32_t ccr = arr/2;
 
     //Setting ARR and CCR1
     if(freq != 0) {
-        TIM16->ARR = arr;
-        TIM16->CCR1 = ccr;
-
-        //Setting the UG but to upload shadow registers
-        TIM16->EGR |= (1 << 0);
-
         //Set the CEN in CR1 - turn on the timer at the end
-        TIM16->CR1 |= (1 << 0);
+        //TIMx->CR1 |= (1 << 0);
+
+        TIMx->ARR = arr;
+        TIMx->CCR1 = ccr;
+
     } else {
-        TIM16->ARR = 0;
-        TIM16->CCR1 = 0;
-        TIM16->CR1 &= ~(1 << 0);
+        TIMx->ARR = 0;
+        TIMx->CCR1 = 0;
+        //TIMx->CR1 &= ~(1 << 0);
     }
+
+    //Setting the UG but to upload shadow registers
+    TIMx->EGR |= (1 << 0);
 
 }
